b0062184d7288af00bfc267dfca80b5d23494ef2
이신일 <itsinil@gmail.com>
2019-12-09 23:13:46 +0900

    expo location 을 이용한 API 호출

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Alert } from 'react-native';
+import Loading from "./Loading"
+import * as Location from "expo-location"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    isLoading: false,
+  }
+  getLocation = async () => {
+    try{
+      await Location.requestPermissionsAsync()
+      const { coords: {latitude, longitude} } = await Location.getCurrentPositionAsync()
+      console.log(latitude, longitude)
+      //Send to API and get weather
+
+      this.setState({isLoading: true});
+    }catch (e) {
+      Alert.alert("Can't find you.", "So sad")
+    }
+  }
+  componentDidMount(){
+    this.getLocation()
+  }
+  render () {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor: "blue"
+  },
+  text: {
+    color: "red"
+  }
 });
