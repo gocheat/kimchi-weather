1f27defc5e8109d877eebbccc281db5af0214810
이신일 <itsinil@gmail.com>
2019-12-09 23:37:34 +0900

    axios 를 이용한 날시 API 호출하기

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View , Alert } from 'react-native';
 import Loading from "./Loading"
 import * as Location from "expo-location"
+import axios from "axios"
+
+const API_KEY = "b7f0fb6cf93c3639475bd1deb62f78c9"
 
 export default class App extends React.Component {
   state = {
     isLoading: false,
+  }
+  //날씨 데이터 가져오기 By 위도,경도
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`)
+    console.log("getWeather", data)
   }
   getLocation = async () => {
     try{
@@ -13,9 +21,10 @@ export default class App extends React.Component {
       const { coords: {latitude, longitude} } = await Location.getCurrentPositionAsync()
       console.log(latitude, longitude)
       //Send to API and get weather
-
+      await this.getWeather(latitude, longitude)
       this.setState({isLoading: true});
     }catch (e) {
+      console.log("error", e)
       Alert.alert("Can't find you.", "So sad")
     }
   }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Fokin Weather
+# Kimchi Weather
 
-Learning React Native by Building a Faking Weather App
+Learning React Native by Building a Kimchi Weather App
+
+##### 날씨 API - https://openweathermap.org/
